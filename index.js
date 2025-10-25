e5a0dacb08ea0460b5407ecef1bc2a9bf29a8efe
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-10-25 16:48:25 +0530

    Refactor task handling and improve button functionality in To-Do App

--- index.js
+++ index.js
@@ -3,24 +3,38 @@ const inputEl = document.querySelector("#TaskInput");
 const addBtnEl = document.querySelector("#addTaskBtn");
 const tasklistEl = document.querySelector("#Tasklist");
 
+// events
 addBtnEl.addEventListener("click",()=>{
   savebtn()
   task()
 })
 
 
+
+// functions
+const inputArray = [];
+inputArray.push(inputEl.value);
 const savebtn = () => {
   console.log("Button was pressed");
+  console.log(inputArray);
+  
 }
 
+//html elements
 const li = document.createElement("li");
-li.className = "deletebtn"
-li.innerHTML = `<span>${tasktext}</span> <button>X</button>` ;
-const task = () => {
-  const inputEl = inputEl.value.trim();
-  if(inputEl === ""){
-    return;
-  }
-  tasklistEl.appendChild(li);
+const deletebtn = document.createElement("button");
+li.className = "taskitem";
+deletebtn.className = "delbtn";
+li.innerHTML = `<span> ${inputArray} </span>`;
+deletebtn.innerHTML = `<button>X</button>`;
 
+// add task function
+const task = () => {
+  tasklistEl.appendChild(li);
+  li.appendChild(deletebtn);
 }
+
+// delete btn
+deletebtn.addEventListener("click", ()=>{
+  tasklistEl.removeChild(li)
+})
