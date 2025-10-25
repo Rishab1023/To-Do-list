d412d575ccb9bf08b3eadfe29942575a1cc1ede3
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-10-25 14:37:11 +0530

    Refactor HTML structure and enhance CSS styles for better layout and task display

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const inputEl = document.querySelector("#TaskInput")
-const addBtnEl = document.querySelector("#addTaskBtn")
-const tasklistEl = document.querySelector("#Tasklist")
+
+const inputEl = document.querySelector("#TaskInput");
+const addBtnEl = document.querySelector("#addTaskBtn");
+const tasklistEl = document.querySelector("#Tasklist");
 
 addBtnEl.addEventListener("click",()=>{
-
+  savebtn()
+  task()
 })
+
+
+const savebtn = () => {
+  console.log("Button was pressed");
+}
+
+const li = document.createElement("li");
+li.className = "deletebtn"
+li.innerHTML = `<span>${tasktext}</span> <button>X</button>` ;
+const task = () => {
+  const inputEl = inputEl.value.trim();
+  if(inputEl === ""){
+    return;
+  }
+  tasklistEl.appendChild(li);
+
+}
